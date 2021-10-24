0329dd2dbe6d6333783f62c6b7fa9f1ae94390ac
MHendricks101 <92061759+MHendricks101@users.noreply.github.com>
2021-10-24 13:53:26 -0500

    Create DiskCleanupUtility.bat

---
+++ DiskCleanupUtility.bat
@@ -0,0 +1,7 @@
+::-------------------------------------------------------------------
+:: This script launches the Disk Cleanup Utility
+:: Selects all options, runs automatically without user input
+::-------------------------------------------------------------------
+
+
+start cleanmgr /verylowdisk /d c:\
